aa395540c06b94125e7971b355105bcc8957bf85
Sangkon Han <sigmadream@gmail.com>
2021-08-16 00:43:09 +0900

    // WIP 헤더, 푸터 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,12 @@
+import { Footer } from './components/Footer';
+import { Header } from './components/Header';
+
 const App = () => {
   return (
     <div>
+      <Header />
       <h1>Hello, MERN-shop</h1>
+      <Footer />
     </div>
   );
 };
