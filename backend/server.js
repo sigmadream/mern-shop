8b41762819c71d2a8dabcf9398c1e023b7227f6a
Sangkon Han <sigmadream@gmail.com>
2021-08-30 03:15:00 +0900

    // Mongodb 및 API

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import products from './data/products.js';
-// const express = require('express');
-// const dotenv = require('dotenv');
-// const products = require('./data/products');
+import colors from 'colors';
+import connectDB from './config/db.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import productRoutes from './routes/productRoutes.js';
 
 dotenv.config();
+
+connectDB();
 
 const app = express();
 
@@ -13,19 +15,15 @@ app.get('/', (req, res) => {
   res.send('API is Running...');
 });
 
-app.get('/api/products', (req, res) => {
-  res.json(products);
-});
-
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use('/api/products', productRoutes);
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(
     `✅ Server running is ${process.env.NODE_ENV} mode on port, http://localhost:${PORT} `
+      .yellow.bold
   );
 });
